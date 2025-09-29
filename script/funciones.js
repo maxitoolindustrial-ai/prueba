69edd1e6f0262431c3450cfa0cbc4460cbd03d93
maxitoolindustrial-ai <maxitoolindustrial@gmail.com>
2025-09-29 00:37:00 -0600

    Delete script directory

--- script/funciones.js
+++
@@ -1,3 +0,0 @@
-function cargapagina(htmlDocument) {
-	document.getElementById("content").innerHTML = '<object width=\"100%\" height=\"100%\" type="text/html" data="' + htmlDocument + '" ></object>';
-}
